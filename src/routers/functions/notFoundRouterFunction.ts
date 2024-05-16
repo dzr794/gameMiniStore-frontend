d14be1dd2b3abe44be9721fd67fc3d31d2f8a6c8
David Zapata <dzr794@gmail.com>
2024-05-15 22:49:17 -0500

    added header to 404 page

--- src/routers/functions/notFoundRouterFunction.ts
+++ src/routers/functions/notFoundRouterFunction.ts
@@ -1,6 +1,10 @@
+import { HeaderModel, HeaderView, mainHeaderData } from "../../components/headers";
 import { NotFoundPageView } from "../../pages";
 
 export const notFoundRouterFunction = () => {
   const notFoundPageView = new NotFoundPageView();
   notFoundPageView.render();
+
+  const headerView = new HeaderView( {el: '#header', model: new HeaderModel} );
+  headerView.render( mainHeaderData );
 }
